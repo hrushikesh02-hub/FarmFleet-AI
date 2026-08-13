b131966aeabb34b9d331c0e4f4cc467fd5ba2d10
hrushikesh02-hub <hrushikeshthombare95@gmail.com>
2026-08-13 19:24:03 +0530

    Final Testing Done

--- backend/server.js
+++ backend/server.js
@@ -138,11 +138,6 @@ app.use("/api/earnings", earningsRoutes);
 ========================== */
 
 app.use("/api/reviews", reviewRoutes);
-
-/* ==========================
-   PAYMENT ROUTES
-========================== */
-
 app.use("/api/payment", paymentRoutes);
 
 /* ==========================
